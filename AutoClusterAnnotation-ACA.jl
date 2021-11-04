51125bd21f110498a480bd817d28b989c139c21a
oHunewald <o.hunewald@gmail.com>
2021-11-04 10:47:56 +0100

    Add files via upload

---
+++ AutoClusterAnnotation-ACA.jl
@@ -0,0 +1,54 @@
+## ----------------------------------------------------------------------------
+##
+##                  ACA - Automated Cluster Annotation
+##
+## ----------------------------------------------------------------------------
+using Pkg;
+Pkg.activate(".")
+# Pkg.instantiate()
+using CSV
+using DataFrames
+using Plots
+using StatsBase
+using XLSX
+include("ACAhelper.jl")
+using .ACAhelper
+
+# -----------------------------------------------------------------------------
+# Main settings
+# -----------------------------------------------------------------------------
+maindir = "" # set the path
+datadir = ""
+outdir = datadir
+
+clusterSize = 1024
+
+exprFile = "median_expr_$clusterSize.csv"
+freqFile = "cluster_freq_$clusterSize.csv"
+cMatrixFile = "PBMC_Cell_Matrix.csv"
+thFile = "marker_thresholds.csv"
+
+annotatedDF = ACAhelper.doAnnotation(maindir, datadir, outdir, exprFile, freqFile , thFile, cMatrixFile)
+
+CSV.write(outdir*"/annotated_df_$clusterSize.csv", annotatedDF)
+
+# create the mean table 
+tmp = annotatedDF[:, Not(:cluster)]
+groupby(tmp, :cell)
+X = combine(groupby(tmp, :cell), 1:35 .=> mean, :size => sum)
+
+CSV.write(outdir*"/mean_marker_expr_df.csv", X)
+# output the cluster merging file_id
+CSV.write(outdir*"/cluster_merging.csv" ,annotatedDF[!, [:cell, :cluster]] |> DataFrame)
+CSV.write(outdir*"/som_df_norm_$clusterSize.csv", annotatedDF)
+
+
+
+
+
+
+
+
+
+
+
